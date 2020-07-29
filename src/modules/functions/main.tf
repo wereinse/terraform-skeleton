127a171237c91218483fa35143c2fbb795825083
Wendy Reinsel <wereinse@microsoft.com>
2020-07-29 14:09:25 -0400

    messed up functions

--- src/modules/functions/main.tf
+++ src/modules/functions/main.tf
@@ -18,83 +18,52 @@
 * ```
 */
 
-resource "random_id" "funcstorage" {
-  byte_length = 6
-  keepers = {
-    rg_id = "${var.FUNCTION_RG}"
-  }
-}
-
-resource "azurerm_kubernetes_cluster" "aks" {
-  for_each                 = var.INSTANCE
-  name                     = substr("${each.key}funcstorage", 0, 24)
-  resource_group_name      = var.FUNCTION_RG
-  location                 = var.LOCATION
-  dns_prefix               = var.NAME 
-  
-  linux_profile {
-    admin_username = "ubuntu"
-
-    ssh_key {
-      key_data = var.SSH_PUBLIC_KEY
-    }
-  }
-
-  default_node_pool {
-    name = "default"
-//    count = 1
-    vm_size = "Standard_DS1_v2"
-//    os_type = "Linux"
-    os_disk_size_gb = "30"
-  }
-
-  service_principal {
-    client_id = var.ACR_SP_ID
-    client_secret = var.ACR_SP_SECRET
-  }
-}
-
-output "id" {
-    value = "${azurerm_kubernetes_cluster.aks.id}"
-}
-
-output "kube_config" {
-  value = "${azurerm_kubernetes_cluster.aks.kube_config_raw}"
-}
-
-output "client_key" {
-  value = "${azurerm_kubernetes_cluster.aks.kube_config.0.client_key}"
-}
-
-output "client_certificate" {
-  value = "${azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate}"
-}
-
-output "cluster_ca_certificate" {
-  value = "${azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate}"
-}
-
-output "host" {
-  value = "${azurerm_kubernetes_cluster.aks.kube_config.0.host}"
-}
-
 resource "azurerm_storage_account" "function-storage-account" {
   for_each                 = var.INSTANCE
-  name                     = substr("${each.key}.random_id.funcstorage.id", 0, 24)
+  name                     = substr("${each.key}funcstorage", 0, 24)
   resource_group_name      = var.FUNCTION_RG
   location                 = var.LOCATION
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
+resource "azurerm_storage_container" "function-deploy-container" {
+    for_each                 = var.INSTANCE
+    name                     = substr("${each.key}funccntr", 0, 24)
+    storage_account_name     = azurerm_storage_account.function-storage-account[each.key].name
+    container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "appcode" {
+    for_each                  = var.INSTANCE
+    name                      = "functionapp.zip"
+    storage_account_name      = azurerm_storage_account.function-storage-account[each.key].name
+    storage_container_name    = azurerm_storage_container.function-deploy-container[each.key].name
+    type                      = "Block"
+    source                    = var.FUNCTION_APP_JSON
+}
+
 resource "azurerm_function_app" "function-app" {
-  for_each                  = var.INSTANCE
-  name                      = "${each.key}-funcapp.random_id.funcstorage.id"
+ // for_each                  = var.INSTANCE
+   depends_on = [var.APPINS_IKEY]
+  name                      = "${each.key}-function"
   location                  = var.LOCATION
   resource_group_name       = var.FUNCTION_RG
-  app_service_plan_id       = module.web.azurerm_app_service_plan.init-functionapp-plan.id 
+  app_service_plan_id       = "var.NAME-${each.key}"
   storage_connection_string = azurerm_storage_account.function-storage-account[each.key].primary_connection_string 
   version                   = "~2"
-  app_settings              = var.AKS_APP_SETTINGS
-  WEBSITE_RUN_FROM_PACKAGE  = var.FUNCTION_APP_JSON
+  app_settings              = var.APPINS_IKEY
+//  WEBSITE_RUN_FROM_PACKAGE     = var.FUNCTION_APP_JSON
 }
+
+resource "azurerm_app_service_plan" "function-service-plan" {
+  for_each                 = var.INSTANCE
+  name                     = "${each.key}-function-service-plan"
+  resource_group_name      = var.FUNCTION_RG
+  location                 = var.LOCATION
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
