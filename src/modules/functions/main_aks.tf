ca791f8f446ab28c81ab8a5408cf729a7dfb9bbe
Wendy Reinsel <wereinse@microsoft.com>
2020-07-30 10:38:04 -0400

    empty list of objects

--- src/modules/functions/main_aks.tf
+++ src/modules/functions/main_aks.tf
@@ -1,41 +1,63 @@
+// resource "random_string" "password" {
+//     length  = 32
+//     special = true
+// }
+
+// resource "azuread_application" "aksapp" {
+//     name = "${var.NAME}-aksapp"
+// }
+
+// resource "azuread_service_principal" "akssp" {
+//     application_id               = azuread_application.aksapp.application_id
+//     app_role_assignment_required = false
+// }
+
+// resource "azuread_service_principal_password" "akssppwd" {
+//     service_principal_id = azuread_service_principal.akssp.id
+//     value                = random_string.password.result
+//     end_date_relative    = "8760h"
+//     lifecycle {
+//         ignore_changes = [
+//             value,
+//             end_date_relative
+//         ]
+//     }
+// }
+
 data "azurerm_subscription" "current" {}
 
 resource "azurerm_kubernetes_cluster" "aks" {
-//  for_each                 = var.INSTANCE 
-//  name                     = substr("${each.key}funcstorage", 0, 24)
-  name                     = "aksmsi"
-  resource_group_name      = var.FUNCTION_RG
-  location                 = var.LOCATION
-  dns_prefix               = var.NAME
-  
-  identity {
-    type                   = "SystemAssigned"
-  }
+    name                = "${var.NAME}-aks"
+    location            = var.LOCATION 
+    resource_group_name = var.FUNCTION_RG
+    dns_prefix          = "${var.NAME}-aks"
+    kubernetes_version  = "1.17.7"
 
-  service_principal {
-    client_id              = var.ACR_SP_ID
-    client_secret          = var.ACR_SP_SECRET
-  }   
+    default_node_pool {
+        name                = "default"
+        vm_size             = "Standard_DS2_v2"
+        enable_auto_scaling = true
+        min_count           = 1
+        max_count           = 30
+        os_disk_size_gb     = 128
+    }
 
-  // linux_profile {
-  //   admin_username              = "ubuntu"
-  //   ssh_key {
-  //     ssh_key   = "~/.ssh/id_rsa.pub"
-  //   }
-  // }
+    identity {
+        type                = "SystemAssigned"
+    }
+    service_principal {
+        client_id     = "msi"
+        client_secret = var.ACR_SP_SECRET
+    }
+}
 
-identity {
-  type          = "SystemAssigned"
-  principal_id  = var.ACR_SP_ID
-  tenant_id     = var.TF_TENANT_ID
-  }
+data "azurerm_role_definition" "contributor" {
+    name                    = "Contributor"         
+}
 
-
-default_node_pool {
-  name = "default"
-  node_count = 1
-  vm_size = "Standard_DS1_v2"
-//  os_type = "Linux"
-  os_disk_size_gb = "30"
-  }
+resource "azurerm_role_assignment" "aksrole" {
+    name                    = azurerm_kubernetes_cluster.aks.name
+    scope                   = data.azurerm_subscription.current.id 
+    role_definition_id      = "$data.azurerm_subscription.subscription.id}$data.azurerm_role_definition.contributor.id}"
+    principal_id            = azurerm_kubernetes_cluster.aks.identity[0].principal_id
 }
--- src/modules/functions/variables.tf
+++ src/modules/functions/variables.tf
@@ -16,19 +16,6 @@ variable "INSTANCE" {
   description = "Map of the environment name and the helium application language to use i.e {myinstance1 = boh, myinstance2 = ibmivcount}"
   type        = map(string)
 }
-variable "FUNCTION_APP_JSON" {
-  description = "Zip file containing the code for the Azure Function"
-  type        = string
-}
-variable "APPINS_IKEY" {
-  description = "Application Insights key used by Function module"
-  type        = string
-}
-variable "TF_TENANT_ID" {
-  type        = string
-  description = "This is the tenant ID of the Azure subscription. This is only used by the parent main.tf"
-}
-
 variable "ACR_SP_ID" {
   type        = string
   description = "The ACR Service Principal ID"
